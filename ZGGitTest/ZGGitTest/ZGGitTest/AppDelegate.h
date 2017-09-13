2fb3db4a3d824f5035cc99e30ed806a9f3b896c0
Gao <838622430@qq.com>
2017-09-13 11:10:53 +0800

    初始化项目

---
+++ ZGGitTest/ZGGitTest/ZGGitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZGGitTest
+//
+//  Created by Gao on 2017/9/13.
+//  Copyright © 2017年 icebartech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
